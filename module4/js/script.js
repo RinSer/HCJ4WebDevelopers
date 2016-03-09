77d99d789626b0e05c6a7130ceee1f7021911318
RinSer <serj.dukareff@gmail.com>
2016-03-09 11:32:23 +0300

    module4_5

--- module4/js/script.js
+++ module4/js/script.js
@@ -94,9 +94,6 @@ $ajaxUtils.sendGetRequest(
 function buildAndShowHomeHTML (categories) {
   
   // Load home snippet page
-  var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-  var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", "'"+chosenCategoryShortName+"'");
-  console.log(homeHtmlToInsertIntoMainPage);
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
@@ -119,14 +116,14 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       // 
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", "'"+chosenCategoryShortName+"'");
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'"+chosenCategoryShortName+"'");
       
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that. 
       // ....
-      insertHtml(homeHtml, homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
